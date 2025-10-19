8e1900d6056a49a3594432c76b5bb866dd4fdcd7
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2025-10-19 11:53:22 +0530

    dbjnjd

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://khatauligud.com", "https://www.khatauligud.com"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
@@ -19,5 +23,5 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
